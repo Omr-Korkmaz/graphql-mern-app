a11414025d640b6b2d3c0d0141eb5470de331d31
omer Korkmaz <h.omr.korkmaz@gmail.com>
2022-04-26 22:50:30 +0200

    basic setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const graphqlHTTP = require('express-graphql'); //convention name
+const cors = require("cors");
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
